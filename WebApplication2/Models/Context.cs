b505f2ae27cb4b5f921d9d2f9413eae0830a080d
S3gillu <shubhamsaurabh440@yahoo.com>
2017-09-27 15:06:07 +0530

    This is crud in mvc

---
+++ WebApplication2/Models/Context.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication2.Models;
+using System.Data.Entity;
+namespace WebApplication2.Models
+{
+    public class Context :Dbcontext
+
+    {
+        public Context()
+            : base("Context")
+        {
+        }
+      public Dbset <employe> Employes { get; set; }
+
+
+    }
+}
---
+++ WebApplication2/Models/employe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class employe
+    {
+
+        public int id { get; set; }
+        public string employename { get; set; }
+        public string employeaddress { get; set; }
+        public string employeemailid { get; set; }
+
+
+    }
+}
